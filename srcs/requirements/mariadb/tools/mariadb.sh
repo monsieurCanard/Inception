749fb72557f228b13dc29b38b45b1f5810120687
Inception <mail.anthony42@gmail.com>
2024-12-13 13:05:52 +0100

    Inception Perfect with docker secret

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,9 +1,21 @@
 #!bin/sh
 
+echo "Starting MariaDB";
 
 /etc/init.d/mariadb start;
 
-mysql_secure_installation << END
+export MY_SQL_PASSWORD=$(cat $MY_SQL_PASSWORD_FILE);
+export MY_SQL_ROOT_PASSWORD=$(cat $MY_SQL_ROOT_PASSWORD_FILE);
+export MY_SQL_USER=$(cat $MY_SQL_USER_FILE);
+export DB_NAME=$(cat $DB_NAME_FILE);
+
+DB_ALREADY_EXISTS=$(mysql -uroot -p$MY_SQL_ROOT_PASSWORD -e "SHOW DATABASES" | grep ${MY_WORDPRESS_DB} | wc -l);
+
+if [ $DB_ALREADY_EXISTS -eq 1 ]; then
+	echo "Database already exists";
+else
+	echo "Database does not exist";
+	mysql_secure_installation << END
 
 Y
 $MY_SQL_ROOT_PASSWORD
@@ -15,14 +27,14 @@ Y
 END
 
 
-#create DB    
-mysql -uroot -p$MY_SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${MY_WORDPRESS_DB};"
+	mysql -uroot -p$MY_SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 
-mysql -uroot -p$MY_SQL_ROOT_PASSWORD -e  "grant all on *.* to 'root'@'%' identified by '${MY_SQL_ROOT_PASSWORD}';FLUSH PRIVILEGES;";
+	mysql -uroot -p$MY_SQL_ROOT_PASSWORD -e  "grant all on *.* to 'root'@'%' identified by '${MY_SQL_ROOT_PASSWORD}';FLUSH PRIVILEGES;";
 
-mysql -uroot -p$MY_SQL_ROOT_PASSWORD -e "CREATE USER '${MY_SQL_USER}'@'%' IDENTIFIED BY '${MY_SQL_PASSWORD}'; GRANT ALL PRIVILEGES ON ${MY_WORDPRESS_DB}.* TO '${MY_SQL_USER}'@'%'; FLUSH 
-PRIVILEGES;";
+	mysql -uroot -p$MY_SQL_ROOT_PASSWORD -e "CREATE USER '${MY_SQL_USER}'@'%' IDENTIFIED BY '${MY_SQL_PASSWORD}'; GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${MY_SQL_USER}'@'%'; FLUSH 
+	PRIVILEGES;";
 
+fi
 
 /etc/init.d/mariadb stop;
 
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -2,6 +2,7 @@
 
 sleep 10
 
+
 if [ -f ./wordpress/wp-config.php ]
 then
 	echo "wordpress already downloaded"
@@ -11,13 +12,14 @@ else
 	rm -rf latest.tar.gz
 
 	cd /var/www/html/wordpress
-	sed -i "s/username_here/${MY_WORDPRESS_DB_USER}/g" wp-config-sample.php
-	sed -i "s/password_here/${MY_WORDPRESS_DB_PASSWORD}/g" wp-config-sample.php
-	sed -i "s/localhost/${MY_WORDPRESS_DB_HOST}/g" wp-config-sample.php
-	sed -i "s/database_name_here/${MY_WORDPRESS_DB}/g" wp-config-sample.php
+
+	sed -i "s/username_here/$(cat $WORDPRESS_DB_USER_FILE)/g" wp-config-sample.php
+	sed -i "s/password_here/$(cat $WORDPRESS_DB_PASSWORD_FILE)/g" wp-config-sample.php
+	sed -i "s/localhost/$(cat $WORDPRESS_DB_HOST_FILE)/g" wp-config-sample.php
+	sed -i "s/database_name_here/$(cat $WORDPRESS_DB_NAME_FILE)/g" wp-config-sample.php
 	mv wp-config-sample.php wp-config.php
 
-	/tmp/wp-cli.phar core install --url=${MY_WORDPRESS_URL} --title=${MY_WORDPRESS_TITLE} --admin_user=${MY_WORDPRESS_ADMIN_USER} --admin_password=${MY_WORDPRESS_ADMIN_PASSWORD} --admin_email=${MY_WORDPRESS_ADMIN_EMAIL} --allow-root
+	/tmp/wp-cli.phar core install --url=$(cat $WORDPRESS_URL_FILE) --title=$(cat $WORDPRESS_TITLE_FILE) --admin_user=$(cat $WORDPRESS_ADMIN_USER_FILE) --admin_password=$(cat $WORDPRESS_ADMIN_PASSWORD_FILE) --admin_email=$(cat $WORDPRESS_ADMIN_EMAIL_FILE) --allow-root
 
 	/tmp/wp-cli.phar user create hen duckduck@example.com --user_pass=hen --allow-root
 fi
