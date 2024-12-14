622023a47b807d3b4f019dbf888ec287c46eefe9
MonsieurC <136458996+monsieurCanard@users.noreply.github.com>
2024-12-14 21:43:09 +0100

    Update ftp_setup.sh

--- srcs/bonus/ftp_server/tools/ftp_setup.sh
+++ srcs/bonus/ftp_server/tools/ftp_setup.sh
@@ -6,7 +6,7 @@ if grep -q "^$(cat $FTP_USER_FILE)):" /etc/passwd; then
 	echo "FTP user already exists";
 else
 	useradd -d /var/www/html $(cat $FTP_PASSWORD_FILE))
-	echo "duck:duck4life" | chpasswd
+	echo "$(cat $FTP_USER_FILE):$(cat $FTP_PASSWORD_FILE)" | chpasswd
 fi
 
 chmod -R 777 /var/www/html
